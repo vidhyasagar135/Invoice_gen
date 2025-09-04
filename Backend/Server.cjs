608dafbcc8cde75345c7aa3661a38bbb38335f0a
vidhyasagar135 <2203A51698@sru.edu.in>
2025-09-04 20:02:23 +0530

    sf

--- Backend/Server.cjs
+++ Backend/Server.cjs
@@ -14,7 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+ origin: process.env.FRONTEND_URL, 
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
